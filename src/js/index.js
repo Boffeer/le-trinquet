ce77fee42633fef6ab17e07d3741eb94a49eabda
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-09-08 10:56:49 +0300

    Делает ленивую загрузку карт

--- src/js/index.js
+++ src/js/index.js
@@ -102,6 +102,44 @@ for (let anchor of anchors) {
   });
 }
 
+// #region lazymap
+function getElementVerticalOffset(el) {
+  if (!el) return;
+
+  let rect = el.getBoundingClientRect();
+  let currentOffset = rect.top;
+
+  return currentOffset;
+}
+
+const lazyClasses = {
+  container: "js_lazy__media",
+  preview: "js_lazy__preview",
+  content: "js_lazy__content",
+  init: "js_lazy-init",
+};
+
+function loadMap(mapContainer) {
+  const { preview, content, init } = lazyClasses;
+
+  if (!mapContainer.classList.contains(init)) {
+    const placeholder = mapContainer.querySelector(`.${preview}`);
+    const map = mapContainer.querySelector(`.${content}`);
+
+    mapContainer.classList.add(init);
+  }
+}
+
+window.addEventListener("scroll", () => {
+  const mapContainer = document.querySelector(".contacts-map");
+  const offset = getElementVerticalOffset(mapContainer);
+  const lazyOffset = 1000;
+
+  if (offset < lazyOffset) {
+    loadMap(mapContainer);
+  }
+});
+// #endregion lazymap
 // Аккордеон
 // const accordions = new DismalModules.Accordions()
 
